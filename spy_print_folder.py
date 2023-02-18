b7af16bb59b3e0b9eed88721f240102e80c5e8dd
rubensalexsander <rubensalexsander0001@gmail.com>
2023-02-18 18:21:41 -0300

    commit 18/02/2023

--- spy_print_folder.py
+++ spy_print_folder.py
@@ -9,20 +9,20 @@ class PyPrinter:
         
         printers = win32print.EnumPrinters(2)
         self.printer_name = printers[self.num][2]
-    
+   
     def new_print(self, path, arqs):
         win32print.SetDefaultPrinter(self.printer_name)
 
         for arq in arqs:
             win32api.ShellExecute(0, "print", arq, None, path, 0)
-
+            print(f'Arq printed: {path+arq} - {self.printer_name}\n')
 
 class PathLookOut:
     def __init__(self, live_path):
         self.live_path = live_path
 
         self.folder_before = os.listdir(self.live_path)
-        self.folder_after = self.folder_before[:]
+        self.folder_after = self.folder_before[:]        
     
     def look(self, time_delay=1):
         self.folder_after = os.listdir(self.live_path)
@@ -34,23 +34,27 @@ class PathLookOut:
             self.folder_before = self.folder_after
             print('Arq removed')
         elif len_fb < len_fa:
-            news_arqs = []
+            new_arqs = []
             for i in self.folder_after:
-                if not i in self.folder_before: news_arqs.append(i)
+                if not i in self.folder_before: new_arqs.append(i)
             self.folder_before = self.folder_after
 
-            print('New arq')
+            print(f'New arq: {new_arqs}')
             
-            return news_arqs
+            return new_arqs
     
         sleep(time_delay)
             
 
 def main():
     #------------------------------------------------
-    spy_folder = r'/home/alex0001/Downloads/pasta01'
-    printer_num = 0
+    spy_folder = r'C:\Users\Admin\Meu Drive (crucilandiapmmg@gmail.com)\imprimir'
+    printer_num = 2
     #------------------------------------------------
+
+    print('- Spy Print Folder RUNNING -\n')
+    print(f'Path spy default: {spy_folder}')
+    print(f'Printer default: {printer_num}\n')
 
     path_lookout = PathLookOut(spy_folder)
     printer = PyPrinter(printer_num)
@@ -59,7 +63,7 @@ def main():
         resp = path_lookout.look()
 
         if resp:
-            printer.new_print(path_lookout, resp)
+            printer.new_print(spy_folder, resp)
 
 if __name__ == '__main__':
     main()
